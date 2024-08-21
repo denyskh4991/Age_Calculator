4c93611bfded578ecae14b8fb26066a0ba9583e0
denyskh4991 <denyskh4991@gmail.com>
2024-08-21 13:02:28 +0200

    Formating code

--- script.js
+++ script.js
@@ -20,22 +20,20 @@ function calculateAge() {
     y3 = y2 - y1;
 
     if (m2 >= m1) {
-        m3 = m2-m1;
-    }
-    else {
+        m3 = m2 - m1;
+    } else {
         y3--;
         m3 = 12 + m2 - m1;
     }
 
-    if(d2 >= d1) {
+    if (d2 >= d1) {
         d3 = d2 - d1;
-    }
-    else {
+    } else {
         m3--;
         d3 = getDaysInMonth(y1, m1) + d2 - d1;
     }
 
-    if(m3 < 0) {
+    if (m3 < 0) {
         m3 = 11;
         y3--;
     }
@@ -44,5 +42,5 @@ function calculateAge() {
 }
 
 function getDaysInMonth(year, month) {
-    return new Date (year, month, 0).getDate();
+    return new Date(year, month, 0).getDate();
 }
